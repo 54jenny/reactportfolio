9ec69db69ff050df1fd8efb6f310c9a3cad567a1
jaeeunlee <j.lee@lionbridge.com>
2019-10-23 00:47:16 +0900

    edit 201910234 ver.

--- src/careers.js
+++ src/careers.js
@@ -14,14 +14,19 @@ class Careers extends Component {
             <div>
                 <h3 className="major">Internship : BITNINE, 2016.03 ~ 2016.09</h3>
                 <p></p>
-                <p>6학기를 마친 후 실무 경험을 쌓기 위해 (주)비트나인 그래프컨설팅팀에서 6개월간 인턴으로 근무했습니다. 주로 Graphic DBMS 관련 문서를 번역하는 것과 Unit test를 포함한 QA 업무를 담당했습니다.
+                <p>GRAPHIC DBMS 관련 문서 번역 및 UNIT TEST를 포함한 QA 업무
                 </p>
                 <br></br>
+				<h3 className="major">Carrer : Lionbridge Korea, 2018.08 ~</h3>
+                <p></p>
+                <p>BIXBY 캡슐 개발 및 유지보수</p>
+				<p>BIXBY TUTORIAL WEB PAGE 개발 및 유지보수</p>
+                <br></br>
                 <h3 className="major">Study #1 : FAST CAMPUS, 2014.08 ~ 2014.10</h3>
-                <p>패스트캠퍼스에서 진행한 웹 프로그래밍 CAMP 1기 강좌를 수료하였습니다. 이 강좌를 통해 Django와 웹 프론트엔드에 대해 기초적인 교육을 받았으며, Python과 HTML+JS+CSS에 대해 조금 알게 되었습니다. 수료 과제로는 Django + Bootstrap을 이용한 풀 스택 게시판 프로젝트를 진행하였습니다.</p>
+                <p>패스트캠퍼스에서 진행한 웹 프로그래밍 CAMP 1기 강좌를 수료. 이 강좌를 통해. Python(Django) + Bootstrap을 이용한 풀 스택(Server + Client) 게시판 프로젝트를 진행.</p>
 
                 <h3 className="major">Study #2 : SEOULACCORD, 2015.12 ~ 2016.01</h3>
-                <p>정부에서 지원하는 숭실대학교 서울어코드 동계 프로그래밍 집중교육을 1개월 동안 받았습니다. 이 수업에서 Java와 JSP, 안드로이드, DB에 대해 더 자세히 배울 수 있었습니다. 뿐만 아니라 현업에서 일하고 계신 강사님들의 실전 경험을 들으며 졸업 전에 인턴을 해보고 싶다는 생각을 하게 되었습니다.</p>
+                <p>Java와 JSP, 안드로이드 교육 수료</p>
                 <br></br>
             </div>
         );
